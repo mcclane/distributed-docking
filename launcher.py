793073d5eb8a738a4f706d2bb4e7340b9c09c34b
McClane Howland <mcclane.howland@gmail.com>
2020-04-29 14:20:22 -0700

    added option to not ssh to every host beforehand

--- launcher.py
+++ launcher.py
@@ -11,6 +11,7 @@ from os import system, makedirs, chdir, path
 def main():
     parser = argparse.ArgumentParser()
     parser.add_argument('--cfg', '--c', type=str, required=True, help="configuration file containing hosts, etc")
+    parser.add_argument('--no-check-hosts', action='store_true', required=False, help="don't check that each host can be ssh'd to. Makes startup faster.")
 
     args = parser.parse_args()
     with open(args.cfg) as f:
@@ -23,7 +24,10 @@ def main():
         cmd.extend(a.split())
 
     cmd.append('--host')
-    reachable_hosts = find_reachable_hosts(cfg['hosts'], cfg['local_workspace'])
+    if not args.no_check_hosts:
+        reachable_hosts = find_reachable_hosts(cfg['hosts'], cfg['local_workspace'])
+    else:
+        reachable_hosts = cfg['hosts']
     hosts = []
     for h in reachable_hosts:
         hosts.extend([h['hostname']] * h['slots'])
